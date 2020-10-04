b5d8b97353427ecd3799cd29dba33b57eb9fed25
vietmazze <dqnguyen2011@gmail.com>
2020-10-04 14:11:03 -0400

    planner

--- pnl-ftx/src/components/plannerComponents/plannerBody.js
+++ pnl-ftx/src/components/plannerComponents/plannerBody.js
@@ -7,7 +7,39 @@ export const PlannerBody = () => {
       <div className="planner-body">
         <div className="planner-header">Productivity Planner</div>
         <div className="planner-quote">quote</div>
-        <div className="planner-content">content</div>
+        <div className="planner-content">
+          <div className="planner-items">
+            <div className="planner-item">
+              <label className="planner-label">First Task</label>
+              <input className="planner-input" type="text" />
+            </div>
+            <div className="planner-item-buttons">
+              <button className="planner-item-button">Button</button>
+              <button className="planner-item-button">Button</button>
+              <button className="planner-item-button">Button</button>
+              <button className="planner-item-button">Button</button>
+            </div>
+          </div>
+
+          <div className="planner-items">
+            <div className="planner-item">
+              <label className="planner-label">Second Task</label>
+              <input className="planner-input" type="text" />
+            </div>
+          </div>
+          <div className="planner-items">
+            <div className="planner-item">
+              <label className="planner-label">Third Task</label>
+              <input className="planner-input" type="text" />
+            </div>
+          </div>
+          <div className="planner-items">
+            <div className="planner-item">
+              <label className="planner-label">Fourth Task</label>
+              <input className="planner-input" type="text" />
+            </div>
+          </div>
+        </div>
         <div className="planner-note">notes</div>
       </div>
     </div>
