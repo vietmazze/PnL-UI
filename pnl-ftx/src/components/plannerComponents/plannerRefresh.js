777db5adbcdc69fce933f7a3a8f4cc58f18a4d6a
vietmazze <dqnguyen2011@gmail.com>
2020-10-23 11:05:34 -0400

    refresh buton

--- pnl-ftx/src/components/plannerComponents/plannerRefresh.js
+++ pnl-ftx/src/components/plannerComponents/plannerRefresh.js
@@ -1,9 +1,26 @@
 import React from "react";
+import firebase from "../../services/firebase";
 
-export const PlannerRefresh = () => {
+export const PlannerRefresh = ({ onClick }) => {
+  const refreshData = () => {
+    console.log("test");
+    const unsub = firebase
+      .firestore()
+      .collection("note")
+      .get()
+      .then((query) => {
+        query.forEach((doc) => {
+          doc.ref.update({
+            extra: "hello ",
+          });
+        });
+      });
+  };
   return (
     <div>
-      <button className="planner-refresh-btn">Refresh</button>
+      <button className="planner-refresh-btn" onClick={refreshData}>
+        Refresh
+      </button>
     </div>
   );
 };
